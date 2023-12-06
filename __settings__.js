3c8c41f7877d2ec1893fa3975f3e261e5437b8c1
romainguerif <all_along@hotmail.fr>
2023-12-06 15:44:52 +0100

    Add files via upload

--- __settings__.js
+++ __settings__.js
@@ -1,6 +1,6 @@
 window.ASSET_PREFIX = "";
 window.SCRIPT_PREFIX = "";
-window.SCENE_PATH = "1800316.json";
+window.SCENE_PATH = "1891959.json";
 window.CONTEXT_OPTIONS = {
     'antialias': false,
     'alpha': false,
@@ -8,7 +8,7 @@ window.CONTEXT_OPTIONS = {
     'preferWebGl2': true,
     'powerPreference': "default"
 };
-window.SCRIPTS = [ 143042018, 143042021, 143042023, 143143399, 143148896, 143157855, 143157879, 143161200, 143161509, 143177813, 143182270, 143182816, 143183397, 143183812, 143184130, 143734035, 143815855, 144497927, 144575659, 144731007, 146918506, 147370613, 147382322, 149934774 ];
+window.SCRIPTS = [ 156534136, 155939131, 155939087, 155939126, 155939075, 155939104, 155939178, 155939156, 155939109, 155939137, 155939138, 155939139, 155939068, 155939124, 155939140, 155939141, 155939148, 155939071, 155939067, 155939129, 155939082, 155939064, 155939122, 155939065, 155939133, 156396358, 156438079, 156534276, 156569929, 156594010 ];
 window.CONFIG_FILENAME = "config.json";
 window.INPUT_SETTINGS = {
     useKeyboard: true,
@@ -18,6 +18,6 @@ window.INPUT_SETTINGS = {
 };
 pc.script.legacy = false;
 window.PRELOAD_MODULES = [
-    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/143702843/1/draco.wasm.js', 'wasmUrl' : 'files/assets/143702844/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/143702842/1/draco.js', 'preload' : true},
-    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/143702840/1/basis.wasm.js', 'wasmUrl' : 'files/assets/143702838/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/143702839/1/basis.js', 'preload' : false},
+    {'moduleName' : 'BASIS', 'glueUrl' : 'files/assets/155939144/1/basis.wasm.js', 'wasmUrl' : 'files/assets/155939142/1/basis.wasm.wasm', 'fallbackUrl' : 'files/assets/155939143/1/basis.js', 'preload' : false},
+    {'moduleName' : 'DracoDecoderModule', 'glueUrl' : 'files/assets/155939145/1/draco.wasm.js', 'wasmUrl' : 'files/assets/155939090/1/draco.wasm.wasm', 'fallbackUrl' : 'files/assets/155939096/1/draco.js', 'preload' : true},
 ];
